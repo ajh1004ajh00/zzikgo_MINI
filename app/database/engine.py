510110ba90d8e7548a2202430e337456d3505db0
JoEunSaem <88969799+joeunseam@users.noreply.github.com>
2025-06-27 08:04:04 +0900

    connect sqlite db

--- app/database/engine.py
+++ app/database/engine.py
@@ -5,9 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-POSTGRE_DATABASE_URL = os.getenv("POSTGRE_DATABASE_URL", "postgresql://postgres:5501@192.168.45.145:5432/postgres")
+SQLITE_DATABASE_URL = os.getenv("SQLITE_DATABASE_URL", "sqlite:///./zzikgo_mini_sqlite.db") # "POSTGRE_DATABASE_URL", "postgresql://postgres:1234@localhost:5434/postgres"
 
-engine = create_engine(POSTGRE_DATABASE_URL)
+engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False}) #POSTGRE_DATABASE_URL
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
