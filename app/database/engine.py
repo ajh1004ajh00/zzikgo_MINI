75cceb38265a16dce8a92a87b4dd36914721505b
JoEunSaem <88969799+joeunseam@users.noreply.github.com>
2025-06-27 09:00:21 +0900

    connect sqlite db

--- app/database/engine.py
+++ app/database/engine.py
@@ -7,7 +7,7 @@ import os
 
 SQLITE_DATABASE_URL = os.getenv("SQLITE_DATABASE_URL", "sqlite:///./zzikgo_mini_sqlite.db") # "POSTGRE_DATABASE_URL", "postgresql://postgres:1234@localhost:5434/postgres"
 
-engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False}) #POSTGRE_DATABASE_URL
+engine = create_engine(SQLITE_DATABASE_URL) #POSTGRE_DATABASE_URL
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
